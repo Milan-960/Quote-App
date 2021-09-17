20baa7a948f7a22a1f6473ace552e8fbd5255c23
Milan Sachani <milan.sachani.ms@gmail.com>
2021-09-17 12:42:47 +0200

    LOGIN AUTH

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,9 +1,12 @@
-import classes from "./MainNavigation.module.css";
 import { NavLink } from "react-router-dom";
 import { useContext } from "react";
+import classes from "./MainNavigation.module.css";
+import AuthContext from "../../Store/authcontext";
 
 const MainNavigation = () => {
   const authCtx = useContext(AuthContext);
+
+  const isLoggedIn = authCtx.isLoggedIn;
 
   return (
     <header className={classes.header}>
@@ -14,29 +17,39 @@ const MainNavigation = () => {
       </div>
       <nav className={classes.nav}>
         <ul>
-          <li>
-            <NavLink to="/quotes" activeClassName={classes.active}>
-              All Quotes
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/newquotes" activeClassName={classes.active}>
-              Add a New Quote
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/login" activeClassName={classes.active}>
-              Login
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/profile" activeClassName={classes.active}>
-              Profile
-            </NavLink>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
+          {isLoggedIn && (
+            <li>
+              <NavLink to="/quotes" activeClassName={classes.active}>
+                All Quotes
+              </NavLink>
+            </li>
+          )}
+          {isLoggedIn && (
+            <li>
+              <NavLink to="/newquotes" activeClassName={classes.active}>
+                Add a New Quote
+              </NavLink>
+            </li>
+          )}
+          {!isLoggedIn && (
+            <li>
+              <NavLink to="/login" activeClassName={classes.active}>
+                Login
+              </NavLink>
+            </li>
+          )}
+          {isLoggedIn && (
+            <li>
+              <NavLink to="/profile" activeClassName={classes.active}>
+                Profile
+              </NavLink>
+            </li>
+          )}
+          {isLoggedIn && (
+            <li>
+              <button>Logout</button>
+            </li>
+          )}
         </ul>
       </nav>
     </header>
